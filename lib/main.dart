e5f678f5d3cfae1fa5d2c225f77d03ad05708d89
BreadEnthusiast <128649247+BreadEnthusiast@users.noreply.github.com>
2024-01-26 20:18:34 +0100

    Wiele funkcjonalności

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'noteScreen.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
@@ -7,6 +7,8 @@ import 'dart:io';
 void main() {
   runApp(const MyApp());
 }
+
+int totalIndex = 0;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -34,23 +36,34 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+//
+
 class _MyHomePageState extends State<MyHomePage> {
+
   final List<ThemeData> availableThemes = [
     ThemeData(
-      primaryColor: const Color.fromARGB(229, 245, 164, 37),
-      backgroundColor: const Color.fromARGB(255,252, 241, 145),
+      colorScheme: const ColorScheme.light(
+        background: Color.fromARGB(255, 252, 241, 145),
+        primary: Color.fromARGB(229, 245, 164, 37),
+      ),
     ),
     ThemeData(
-      primaryColor: Colors.pink,
-      backgroundColor: Colors.pink[100],
+      colorScheme: ColorScheme.light(
+        background: Colors.pink[100] as Color,
+        primary: Colors.pink,
+      ),
     ),
     ThemeData(
-      primaryColor: Colors.blue,
-      backgroundColor: Colors.blue[100],
+      colorScheme: ColorScheme.light(
+        background: Colors.blue[100] as Color,
+        primary: Colors.blue,
+      ),
     ),
     ThemeData(
-      primaryColor: Colors.grey[400],
-      backgroundColor: Colors.white30,
+      colorScheme: ColorScheme.light(
+        background: Colors.white30,
+        primary: Colors.grey[400] as Color,
+      ),
     ),
   ];
 
@@ -60,12 +73,15 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  late ThemeData selectedTheme;
+  late ThemeData selectedTheme = availableThemes[0];
+  List<Note?> notes = [];
 
   @override
   void initState() {
     super.initState();
     _loadSelectedTheme();
+    _loadNotes();
+    initializeIndex();
   }
 
   // Load selected theme from shared preferences
@@ -82,14 +98,75 @@ class _MyHomePageState extends State<MyHomePage> {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt('selectedThemeIndex', themeIndex);
   }
+
+  Future<void> initializeIndex() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String notesFolderPath = '${documentsDirectory.path}/noteFiles';
+
+    if (await Directory(notesFolderPath).exists()) {
+      List<FileSystemEntity> noteFiles = Directory(notesFolderPath).listSync();
+      totalIndex = noteFiles.length;
+      debugPrint("Current totalIndex: $totalIndex");
+    }
+  }
+
   void _handleThemeChanged(ThemeData newTheme) {
     setState(() {
       selectedTheme = newTheme;
     });
   }
 
+  Future<void> _loadNotes() async {
+    List<Note> loadedNotes = [];
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String notesFolderPath = '${documentsDirectory.path}/noteFiles';
+
+    if (await Directory(notesFolderPath).exists()) {
+      List<FileSystemEntity> noteFiles = Directory(notesFolderPath).listSync();
+
+      for (FileSystemEntity file in noteFiles) {
+        String title = await _readTitleFromFile(file);
+        loadedNotes.add(Note(title: title, filePath: file.path));
+      }
+    }
+    setState(() {
+      notes = loadedNotes;
+    });
+  }
+
+  Future<String> _readTitleFromFile(FileSystemEntity file) async {
+    String content = await File(file.path).readAsString();
+    // Split the content at the first line break to get the title
+    return content.split('\n').first;
+  }
+
+  late File noteFile;
+
+  Future<void> _navigateToNoteScreen() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String notesFolderPath = '${documentsDirectory.path}/noteFiles';
+
+    await Directory(notesFolderPath).create(recursive: true);
+
+    int index = notes.length;
+    String fileName = '$index';
+    File noteFile = File('$notesFolderPath/$fileName');
+    debugPrint('!!!!!!!!!!!!!!!!!$noteFile');
+    await noteFile.writeAsString('');
+
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NoteScreen(onThemeChanged: _handleThemeChanged, noteFile: noteFile,)),
+    );
+    debugPrint(result);
+    _loadNotes(); // Refresh notes when returning from NoteScreen
+  }
+
+
   @override
   Widget build(BuildContext context) {
+    initializeIndex();
+    debugPrint("${notes.length}");
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -119,17 +196,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: Container(
-          color: selectedTheme.backgroundColor,
-      ),
+      body: _buildBody(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => NoteScreen(onThemeChanged: _handleThemeChanged),
-              )
-          );
-        },
+        onPressed: _navigateToNoteScreen,
         backgroundColor: Colors.blue,
         child: const Icon(Icons.add), // You can customize the button color
       ),
@@ -137,19 +206,91 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Future<void> _removeNote(int index) async {
+    File file = File(notes[index]!.filePath);
+    await file.delete();
+    await _loadNotes();
+  }
+
+  Widget _buildBody() {
+    if (notes.isNotEmpty) {
+      return Container(
+        color: selectedTheme.colorScheme.background,
+        child: Column(
+          children: [
+            const SizedBox(height: 8.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: notes.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Note? note = notes[index];
+                  return Dismissible(
+                      key: UniqueKey(),onDismissed: (direction) {
+                    _removeNote(index);
+                  },
+                      child: GestureDetector(
+                        onTap: () {
+                          if (note != null) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => NoteScreen(
+                                  onThemeChanged: onThemeChanged,
+                                  initialNote: note,
+                                ),
+                              ),
+                            );
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            margin: const EdgeInsets.only(bottom: 8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            child: Text(
+                              note?.title ?? "",
+                              style: const TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                        ),
+                      )
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      // Display default content when there are no notes
+      return Container(
+        color: selectedTheme.colorScheme.background,
+        child: const Center(
+          child: Text(
+            "No notes available",
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
+      );
+    }
+  }
   // function to get theme name
   String _getThemeName(ThemeData theme) {
     if (theme.primaryColor == const Color.fromARGB(229, 245, 164, 37) &&
         theme.backgroundColor == const Color.fromARGB(255, 252, 241, 145)) {
       return 'Yellow';
-  }  else if (theme.primaryColor == Colors.pink && theme.backgroundColor == Colors.pink[100]) {
-  return 'Pink';
-  } else if (theme.primaryColor == Colors.blue && theme.backgroundColor == Colors.blue[100]) {
-  return 'Blue';
-  } else if (theme.primaryColor == Colors.grey[400] && theme.backgroundColor == Colors.white30) {
-  return 'White';
-  } else {
-  return 'Unknown';
-  }
+    }  else if (theme.primaryColor == Colors.pink && theme.backgroundColor == Colors.pink[100]) {
+      return 'Pink';
+    } else if (theme.primaryColor == Colors.blue && theme.backgroundColor == Colors.blue[100]) {
+      return 'Blue';
+    } else if (theme.primaryColor == Colors.grey[400] && theme.backgroundColor == Colors.white30) {
+      return 'White';
+    } else {
+      return 'Unknown';
+    }
   }
 }
--- lib/noteScreen.dart
+++ lib/noteScreen.dart
@@ -4,43 +4,78 @@ import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'dart:async';
 
+class Note {
+  final String title;
+  final String filePath;
+
+
+  Note({required this.title, required this.filePath});
+}
+
+
 
 class NoteScreen extends StatefulWidget {
   final Function(ThemeData) onThemeChanged;
-  const NoteScreen({Key? key, required this.onThemeChanged}) : super(key: key);
+  final Note? initialNote;
+  final File? noteFile;
+  const NoteScreen({Key? key, required this.onThemeChanged, this.noteFile, this.initialNote}) : super(key: key);
 
   @override
   _NoteScreenState createState() => _NoteScreenState();
 }
 
+
 class _NoteScreenState extends State<NoteScreen> {
-  late ThemeData selectedTheme;
   final TextEditingController titleController = TextEditingController();
   final TextEditingController noteController = TextEditingController();
 
   final List<ThemeData> availableThemes = [
     ThemeData(
-      primaryColor: const Color.fromARGB(229, 245, 164, 37),
-      backgroundColor: const Color.fromARGB(255, 252, 241, 145),
+      colorScheme: const ColorScheme.light(
+        background: Color.fromARGB(255, 252, 241, 145),
+        primary: Color.fromARGB(229, 245, 164, 37),
+      ),
     ),
     ThemeData(
-      primaryColor: Colors.pink,
-      backgroundColor: Colors.pink[100],
+      colorScheme: ColorScheme.light(
+        background: Colors.pink[100] as Color,
+        primary: Colors.pink,
+      ),
     ),
     ThemeData(
-      primaryColor: Colors.blue,
-      backgroundColor: Colors.blue[100],
+      colorScheme: ColorScheme.light(
+        background: Colors.blue[100] as Color,
+        primary: Colors.blue,
+      ),
     ),
     ThemeData(
-      primaryColor: Colors.grey[400],
-      backgroundColor: Colors.white30,
+      colorScheme: ColorScheme.light(
+        background: Colors.white30,
+        primary: Colors.grey[400] as Color,
+      ),
     ),
   ];
+
+  late ThemeData selectedTheme = availableThemes[0];
+  Timer? _checkTypingTimer;
+  late File filePath;
 
   @override
   void initState() {
     super.initState();
     _loadSelectedTheme();
+    if (widget.noteFile != null) {
+      filePath = widget.noteFile!;
+    } else if (widget.initialNote != null) {
+      titleController.text = widget.initialNote!.title;
+      noteController.text = _readNoteFromFile(widget.initialNote!.filePath);
+      filePath = File(widget.initialNote!.filePath);
+    }
+  }
+
+  String _readNoteFromFile(String filePath) {
+    debugPrint(filePath);
+    return File(filePath).readAsStringSync();
   }
 
   // Load selected theme from shared preferences
@@ -65,6 +100,9 @@ class _NoteScreenState extends State<NoteScreen> {
       appBar: AppBar(
         title: TextField(
           controller: titleController,
+            onChanged: (String value) {
+              resetTimer();
+            },
           style: const TextStyle(color: Colors.black),
           decoration: const InputDecoration(
             hintText: 'Enter note title',
@@ -95,38 +133,66 @@ class _NoteScreenState extends State<NoteScreen> {
         ],
       ),
       body: Container(
-        color: selectedTheme.backgroundColor,
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: TextField(
-                  controller: noteController,
-                  maxLines: null,
-                  style: const TextStyle(color: Colors.black),
-                ),
-              )
-            )
-          ]
-        )
+          color: selectedTheme.colorScheme.background,
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: SingleChildScrollView(
+                      child: TextField(
+                          controller: noteController,
+                          maxLines: null,
+                          style: const TextStyle(color: Colors.black),
+                          onChanged: (String value) {
+                            resetTimer();
+                          }
+                      ),
+                    )
+                )
+              ]
+          )
       ),
     );
   }
 
+  Future<void> _saveNote() async {
+    String title = titleController.text;
+    String note = noteController.text;
+
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String notesFolderPath = '${documentsDirectory.path}/noteFiles';
+
+    await Directory(notesFolderPath).create(recursive: true);
+    File noteFile = filePath;
+
+    await noteFile.writeAsString('$title\n$note');
+    debugPrint('Note saved to: ${noteFile.path}');
+  }
+
+  startTimer() {
+    _checkTypingTimer = Timer(const Duration(milliseconds: 600), () {
+      _saveNote();
+    });
+  }
+
+  resetTimer() {
+    _checkTypingTimer?.cancel();
+    startTimer();
+  }
+
   String _getThemeName(ThemeData theme) {
     if (theme.primaryColor == const Color.fromARGB(229, 245, 164, 37) &&
-        theme.backgroundColor == const Color.fromARGB(255, 252, 241, 145)) {
+        theme.colorScheme.background == const Color.fromARGB(255, 252, 241, 145)) {
       return 'Yellow';
     } else if (theme.primaryColor == Colors.pink &&
-        theme.backgroundColor == Colors.pink[100]) {
+        theme.colorScheme.background == Colors.pink[100]) {
       return 'Pink';
     } else if (theme.primaryColor == Colors.blue &&
-        theme.backgroundColor == Colors.blue[100]) {
+        theme.colorScheme.background == Colors.blue[100]) {
       return 'Blue';
     } else if (theme.primaryColor == Colors.grey[400] &&
-        theme.backgroundColor == Colors.white30) {
+        theme.colorScheme.background == Colors.white30) {
       return 'White';
     } else {
       return 'Unknown';
